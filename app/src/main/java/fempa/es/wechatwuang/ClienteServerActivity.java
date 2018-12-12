140ea171dc1f3ea002bdbda7bcb1de240243f982
Emilio José <emilio.jose.pradera@gmail.com>
2018-12-12 20:03:35 +0100

    Initial commit

---
+++ app/src/main/java/fempa/es/wechatwuang/ClienteServerActivity.java
@@ -0,0 +1,13 @@
+package fempa.es.wechatwuang;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ClienteServerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cliente_server);
+    }
+}
